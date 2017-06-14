a9b19a8a7c0e9f93bfd9c5f660293759def8a9ca
Leon Fassbinder <le.fb98@gmail.com>
2017-06-14 14:23:28 +0200

    test

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		DrachenGui jake = new DrachenGui(new Dragon(16, 1, new Color(181,0,0)));
 		jake.AddDragon(new Dragon(16,2, new Color(0,0,0)));
 		jake.AddDragon(new Dragon(16,3, new Color(19,0,170)));
